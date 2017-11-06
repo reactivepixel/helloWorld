c8158a9db0064c284138121e7984d37f8e851196
Chris Chapman <Chapman@apextion.com>
2017-11-06 11:52:17 -0500

    added logging

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+  console.log('incomming request');
   res.send('Hello World');
+
 });
 
 app.listen(3000, () => {
